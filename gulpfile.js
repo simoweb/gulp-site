058206c60099b1f7c496458de640628ad9f879fa
simone <simone.puliti@gmail.com>
2015-04-12 22:06:01 +0200

    Clean export bower components

--- gulpfile.js
+++ gulpfile.js
@@ -9,76 +9,56 @@ var rename = require('gulp-rename');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 //styles
-var minifyCSS = require('gulp-minify-css');
-var combineMq = require('gulp-combine-mq');
-
-var gulpFilter = require('gulp-filter');
-var postcss      = require('gulp-postcss');
+var cmq = require('gulp-combine-media-queries');
 var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer-core');
+var minifyCSS = require('gulp-minify-css');
+var postcss = require('gulp-postcss');
+var filter = require('gulp-filter');
+var cssbeautify = require('gulp-cssbeautify');
 var mainBowerFiles = require('main-bower-files');
 
-var bowerDir = 'vendor/';
 
-//path scss
-var srcSass = 'assets/styles/*.scss';
+/******************************
+//path
+******************************/
+var srcSass = 'assets/styles/';
 var srcJs = 'assets/js/*.js';
 //destination folder
 var dest = 'assets/vendor/';
+var bowerDir = 'bower_components/';
 
 
-//estraggo i componenti js bower 
-gulp.task('js', function() {
- 	gulp.src(mainBowerFiles())
-		.pipe(gulpFilter('*.js'))
-		//.pipe(concat('test.js'))
-		.pipe(gulp.dest(dest + 'js'));
- });
-
-//estraggo i componenti css bower
-gulp.task('bootstrap', function() {
- 	gulp.src(bowerDir + 'bootstrap/dist/**/')
-	.pipe(gulp.dest('./dist/bootstrap'));
- });
-
-
-gulp.task('icons', function() {
- 	gulp.src(bowerDir + 'fontawesome/fonts/**.*')
-		.pipe(gulp.dest('./dist/fonts'));
- });
-
-
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src(srcJs)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
+/******************************
+//main
+******************************/
 // Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src(srcSass)
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css'));
+gulp.task('styles', function() {
+	//nested
+	gulp.src(srcSass+'main.scss')
+    .pipe(sass())
+	.pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+	.pipe(cmq({
+      log: true
+    }))
+	.pipe(cssbeautify({
+            indent: '  ',
+            openbrace: 'end-of-line',
+            autosemicolon: false
+        }))
+    .pipe(gulp.dest('dist/css'));
+    //minify
+	gulp.src(srcSass+'main.scss')
+    .pipe(sass())
+	.pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+	.pipe(minifyCSS({keepBreaks:false}))
+	.pipe(rename('main.min.css'))
+    .pipe(gulp.dest('dist/css'));
 });
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src(srcJs)
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist/js'))
-        .pipe(rename('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
-
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch(srcJs, ['lint', 'scripts']);
-    gulp.watch(srcSass, ['sass']);
-});
-
+/******************************
 //images min
+******************************/
 gulp.task('comprimeImage', function () {
     return gulp.src('assets/images/*')
         .pipe(imagemin({
@@ -90,56 +70,86 @@ gulp.task('comprimeImage', function () {
 });
 
 
-/*// merge media query
-gulp.task('combineMq', function () {
-    return gulp.src('assets/css/*')
-    .pipe(combineMq({
-        beautify: true
+/******************************
+//script complie and minify
+******************************/
+gulp.task('scripts', function() {
+    return gulp.src(srcJs)
+        .pipe(gulp.dest('dist/js'))
+        .pipe(rename('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+/******************************
+//link task
+******************************/
+gulp.task('lint', function() {
+    return gulp.src(srcJs)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+/******************************
+//wach for change
+******************************/
+gulp.task('watch', function() {
+    gulp.watch(srcJs, ['lint', 'scripts']);
+    gulp.watch(srcSass, ['sass']);
+});
+
+
+/******************************
+//components from bower
+******************************/
+gulp.task('libs', function() {
+
+	var jsFilter = filter(['*.js','!doc-ready.js','!EventEmitter.js','!eventie.js','!get-size.js','!get-style-property.js','!item.js','!matches-selector.js','!outlayer.js']);
+    var cssFilter = filter('*.css');
+    var fontFilter = filter(['*.eot', '*.woff', '*.svg', '*.ttf']);
+
+	return gulp.src(mainBowerFiles())
+
+	// grab vendor js files from bower_components, minify and push in /dist
+	.pipe(jsFilter)
+	//.pipe(gulp.dest('./dist/js/'))
+	.pipe(uglify())
+	.pipe(rename({
+        suffix: ".min"
     }))
-    .pipe(gulp.dest('dist/css'));
-});*/
+	.pipe(gulp.dest('./dist/js/'))
+	.pipe(jsFilter.restore())
 
-// CSS 
-gulp.task('styles', function() {
-	//main.css
-	gulp.src(['./assets/css/main.css'])
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-	.pipe(gulp.dest('./dist/css/'));
+	// grab vendor css files from bower_components, minify and push in /dist
+	.pipe(cssFilter)
+	//.pipe(gulp.dest('./dist/css'))
+	.pipe(minifyCSS())
+	.pipe(rename({
+        suffix: ".min"
+    }))
+	.pipe(gulp.dest('./dist/css'))
+	.pipe(cssFilter.restore())
 
-	//main.css minify
-	gulp.src(['./assets/css/main.css'])
-	.pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-	.pipe(minifyCSS({keepBreaks:false}))
-	.pipe(rename('main.min.css'))
-	.pipe(gulp.dest('./dist/css/'));
-	
-	//bootstrap
-	gulp.src(['./assets/css/bootstrap.css'])
-	.pipe(gulp.dest('./dist/css/'));
-	
-	//bootstrap minify
-	gulp.src(['./assets/css/bootstrap.css'])
-	.pipe(minifyCSS({keepBreaks:false}))
-	.pipe(rename('bootstrap.min.css'))
-	.pipe(gulp.dest('./dist/css/'));
+	// grab vendor font files from bower_components and push in /public 
+	.pipe(fontFilter)
+	//.pipe($flatten())
+	.pipe(gulp.dest('./dist/fonts'))
 });
-//css merge
-//merge all file 
-gulp.task('merge', function() {
-	//all
-	gulp.src(['./assets/css/bootstrap.css', './assets/css/main.css'])
-	.pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-	.pipe(concat('all.css'))
-	.pipe(gulp.dest('./dist/css/'));
-	//minify all
-	gulp.src(['./assets/css/bootstrap.css', './assets/css/main.css'])
-	.pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-	.pipe(minifyCSS({keepBreaks:false}))
-	.pipe(concat('all.min.css'))
-	.pipe(gulp.dest('./dist/css/'));
+
+/******************************
+//compile html
+******************************/
+
+var inject = require('gulp-inject');
+
+gulp.task('index', function () {
+  gulp.src('./dist/*.html')
+  .pipe(inject(gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false}), {relative: true}))
+  .pipe(gulp.dest('./dist'));
 });
+
 
 
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts' ,'comprimeImage','styles','merge','js','bootstrap','icons']);
+gulp.task('default', ['styles','comprimeImage','scripts','lint','libs','index']);
